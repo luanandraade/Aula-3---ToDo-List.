3e48557b55899e1919a98a321de536f5eea7823a
unisanta <unisanta@M312-16>
2023-02-27 21:29:59 -0300

    Add project files.

---
+++ TODO. LIST/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TODO.LIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblTarefa = new System.Windows.Forms.Label();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.dateTimeData = new System.Windows.Forms.DateTimePicker();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(108, 157);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 108);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(105, 71);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(30, 13);
+            this.lblData.TabIndex = 1;
+            this.lblData.Text = "Data";
+            this.lblData.Click += new System.EventHandler(this.lblData_Click);
+            // 
+            // lblTarefa
+            // 
+            this.lblTarefa.AutoSize = true;
+            this.lblTarefa.Location = new System.Drawing.Point(105, 115);
+            this.lblTarefa.Name = "lblTarefa";
+            this.lblTarefa.Size = new System.Drawing.Size(38, 13);
+            this.lblTarefa.TabIndex = 2;
+            this.lblTarefa.Text = "Tarefa";
+            this.lblTarefa.Click += new System.EventHandler(this.lblTarefa_Click);
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(363, 87);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 3;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(355, 295);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 4;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(149, 115);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(208, 20);
+            this.txtTarefa.TabIndex = 5;
+            this.txtTarefa.TextChanged += new System.EventHandler(this.txtTarefa_TextChanged);
+            // 
+            // dateTimeData
+            // 
+            this.dateTimeData.Location = new System.Drawing.Point(149, 71);
+            this.dateTimeData.Name = "dateTimeData";
+            this.dateTimeData.Size = new System.Drawing.Size(208, 20);
+            this.dateTimeData.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.dateTimeData, "Preencha os dados.");
+            this.dateTimeData.ValueChanged += new System.EventHandler(this.dateTimeData_ValueChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimeData);
+            this.Controls.Add(this.txtTarefa);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.lblTarefa);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblTarefa;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.DateTimePicker dateTimeData;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ TODO. LIST/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODO.LIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimeData_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTarefa_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTarefa_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void lblData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtTarefa.Text == "")
+            { 
+                MessageBox.Show("Erro");
+            }
+            else
+            {
+                listBox1.Items.Add($"{dateTimeData.Value:d} - {txtTarefa.Text}");
+            }
+            
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (txtTarefa.Text == "")
+            {
+                MessageBox.Show("Erro");
+            }
+            else
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
